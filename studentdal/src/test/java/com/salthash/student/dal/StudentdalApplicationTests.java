44c6b1c3b4eac551e631f0f0f2f4bbbed8679c4a
Hitoshi5858 <salt.hash329@gmail.com>
2020-05-04 15:59:34 +0900

    20

---
+++ studentdal/src/test/java/com/salthash/student/dal/StudentdalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salthash.student.dal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
