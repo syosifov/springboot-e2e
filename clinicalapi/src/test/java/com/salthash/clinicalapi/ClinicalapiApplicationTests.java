249e2296de9972be2e2f4cfad97d0584757e5380
Hitoshi5858 <salt.hash329@gmail.com>
2020-05-05 11:53:39 +0900

    207

---
+++ clinicalapi/src/test/java/com/salthash/clinicalapi/ClinicalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salthash.clinicalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
