bfeb5aeb1b76dfcdf9636e299755168586035a9a
Hitoshi5858 <salt.hash329@gmail.com>
2020-05-05 11:35:05 +0900

    206 Original Jar

---
+++ flightcheckin/src/test/java/com/salthash/fightcheckin/FlightcheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salthash.fightcheckin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightcheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
